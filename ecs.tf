0f54579849c13b0f6e24d0914263d4965cf588be
Ethan Pinto <ethan.pinto@gmail.com>
2025-04-13 18:56:39 +1000

    Created prac code.

--- ecs.tf
+++ ecs.tf
@@ -45,18 +45,23 @@ DEFINITION
 }
 
 resource "aws_ecs_service" "taskoverflow" {
-  name            = "taskoverflow"
-  cluster         = aws_ecs_cluster.taskoverflow.id
-  task_definition = aws_ecs_task_definition.taskoverflow.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
+    name            = "taskoverflow"
+    cluster         = aws_ecs_cluster.taskoverflow.id
+    task_definition = aws_ecs_task_definition.taskoverflow.arn
+    desired_count   = 1
+    launch_type     = "FARGATE"
 
-  network_configuration {
-    subnets             = data.aws_subnets.private.ids
-    security_groups     = [aws_security_group.taskoverflow.id]
-    assign_public_ip    = true
-  }
+    network_configuration {
+        subnets             = data.aws_subnets.private.ids
+        security_groups     = [aws_security_group.taskoverflow.id]
+        assign_public_ip    = true
+    }
 
+    load_balancer { 
+        target_group_arn = aws_lb_target_group.taskoverflow.arn 
+        container_name   = "taskoverflow" 
+        container_port   = 6400 
+    }
 }
 
 resource "aws_security_group" "taskoverflow" {
